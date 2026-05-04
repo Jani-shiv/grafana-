9768cc2cdbc932f6ac23884cdd98122e472acb8c
Stellarmind <stellarmind@github.local>
2026-05-04 14:26:48 +0530

    fix: downgrade eslint to v8, fix config, remove unused code

--- app.js
+++ app.js
@@ -137,15 +137,6 @@ function getCartSummary() {
   };
 }
 
-function escapeHtml(value) {
-  return String(value)
-    .replaceAll('&', '&amp;')
-    .replaceAll('<', '&lt;')
-    .replaceAll('>', '&gt;')
-    .replaceAll('"', '&quot;')
-    .replaceAll("'", '&#39;');
-}
-
 function renderStorefront() {
   const productSeed = JSON.stringify(products).replaceAll('<', '\\u003c');
 
